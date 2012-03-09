02da9f40137f7e112a168c8a6a46379eb6a9ef56
Antonin Lacombe <antonin.lacombe@ripplemotion.fr>
2012-03-09 16:16:15 +0100

    made swingtime as django app

--- setup.py
+++ setup.py
@@ -1,30 +1,13 @@
-#!/usr/bin/env python
 from setuptools import setup, find_packages
 
-#from distutils.core import setup
-
-
-VERSION = '1.0' 
-
-README_FILE = open('README.markdown')
-try:
-    long_description = README_FILE.read()
-finally:
-    README_FILE.close()
-
- 
 setup(
-    name='django-swingtime',
-    version=VERSION,
-    url='http://github.com/squarefactor/django-swingtime.git',
-    description='Swingtime is a Django calendar application.',
-    long_description=long_description,
-    author='David A Krauth',
-    platforms=['any'],
+   name = "django-swingtime",
+   version = "0.1-dev",
+   description = "planning app for django.",
+   author = 'Lacombe Antonin',
    packages = find_packages('src'),
    package_dir = {'': 'src'},
-    # packages=[
-    #     'swingtime',
-    #     'swingtime.conf'
-    # ],
+   install_requires = [
+      'setuptools',
+   ],
 )
---
+++ src/swingtime/tests/__init__.py
@@ -0,0 +1,5 @@
+
+
+
+from swingtime.tests.sample_tests import *
+
